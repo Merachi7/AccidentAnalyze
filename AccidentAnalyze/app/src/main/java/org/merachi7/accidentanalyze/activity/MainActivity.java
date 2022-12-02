b2cf80bc54fd9920791de282b18fafdfcc7aa5f4
guetory.uni@gmail.com <merachi7@gmail.com>
2022-12-02 22:52:18 +0900

    스플래시에서 메인 액티비티로 3초 후 전환

---
+++ AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package org.merachi7.accidentanalyze.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.merachi7.accidentanalyze.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/SplashActivity.java
+++ AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package org.merachi7.accidentanalyze.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import org.merachi7.accidentanalyze.R;
 
@@ -12,5 +14,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        moveMain(2);
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //new Intent(현재 context, 이동할 activity)
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        }, 1000 * sec);
     }
 }
