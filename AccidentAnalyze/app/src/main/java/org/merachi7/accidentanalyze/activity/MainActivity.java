556bc46c8cc4ef8e83d536ab06925d55cd64788c
guetory.uni@gmail.com <merachi7@gmail.com>
2022-12-04 13:50:24 +0900

    스플래시 이미지 추가,  Preference 수정

--- AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/MainActivity.java
+++ AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/MainActivity.java
@@ -1,16 +1,38 @@
 package org.merachi7.accidentanalyze.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.merachi7.accidentanalyze.R;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //openFile();
+    }
+
+    public void openFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            Uri fileUri = data.getData();
+            String path = fileUri.getPath();
+        }
     }
 }
