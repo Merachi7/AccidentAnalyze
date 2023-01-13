e842742adce9253f2fbae7970ba7cf7eef75696e
Mearchi7 <merachi7@gmail.com>
2023-01-14 01:51:39 +0900

    사고 원인이 해결 방안에 같이 뜨도록 수정, 개수 카운트

--- AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/MethodActivity.java
+++ AccidentAnalyze/app/src/main/java/org/merachi7/accidentanalyze/activity/MethodActivity.java
@@ -16,6 +16,7 @@ import org.merachi7.accidentanalyze.util.CsvHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class MethodActivity extends AppCompatActivity {
@@ -60,20 +61,34 @@ public class MethodActivity extends AppCompatActivity {
 
         textView.setText("공종분류 : " + construction + " / 작업프로세스 : " + process + "\n위험발생객체 : " + hazard + " / 위험발생위치 : " + hazard_position + "\n인적피해 : " + damage);
         List<String> method_list = new ArrayList<>();
+        HashMap<String , Integer> method_cnt = new HashMap<>();
 
         for(int i =0; i < dataList.size(); i++){
             if(!Arrays.asList(construction).contains(dataList.get(i)[4]) || !Arrays.asList(process).contains(dataList.get(i)[11]) || !Arrays.asList(hazard).contains(dataList.get(i)[5]) || !Arrays.asList(hazard_position).contains(dataList.get(i)[6]) || !Arrays.asList(damage).contains(dataList.get(i)[13]))
                 continue;
 
-            if(!method_list.contains(dataList.get(i)[18]))
-                method_list.add(dataList.get(i)[18]);
+            if(!method_list.contains("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18])){
+                method_cnt.put("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18] , 1);
+                method_list.add("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18]);
+            }else{
+                method_cnt.put("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18] , method_cnt.get("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18]) + 1);
+            }
+        }
+
+        List<String> real_method_list = new ArrayList<>();
+
+        for(int i =0; i < method_list.size(); i++){
+            String tmp_save = method_list.get(i);
+            method_list.remove(i);
+
+            real_method_list.add(tmp_save + " (" + method_cnt.get(tmp_save) + ")");
         }
 
         if(method_list.size() == 0){
             method_list.add("선택한 분류에 해당되는 데이터가 없습니다.");
         }
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, method_list) ;
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, real_method_list) ;
 
         listView.setAdapter(adapter) ;
     }
@@ -97,25 +112,39 @@ public class MethodActivity extends AppCompatActivity {
 
 
             textView = (TextView) findViewById(R.id.textCurrentSelect) ;
-            textView.setText("공종분류 : " + construction + "\n작업프로세스 : " + process + "\n위험발생객체 : " + hazard + "\n위험발생위치 : " + hazard_position + "\n인적피해 : " + damage);
             listView = (ListView) findViewById(R.id.method_list);
 
 
+            textView.setText("공종분류 : " + construction + " / 작업프로세스 : " + process + "\n위험발생객체 : " + hazard + " / 위험발생위치 : " + hazard_position + "\n인적피해 : " + damage);
             List<String> method_list = new ArrayList<>();
+            HashMap<String , Integer> method_cnt = new HashMap<>();
 
             for(int i =0; i < dataList.size(); i++){
                 if(!Arrays.asList(construction).contains(dataList.get(i)[4]) || !Arrays.asList(process).contains(dataList.get(i)[11]) || !Arrays.asList(hazard).contains(dataList.get(i)[5]) || !Arrays.asList(hazard_position).contains(dataList.get(i)[6]) || !Arrays.asList(damage).contains(dataList.get(i)[13]))
                     continue;
 
-                if(!method_list.contains(dataList.get(i)[18]))
-                    method_list.add(dataList.get(i)[18]);
+                if(!method_list.contains("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18])){
+                    method_cnt.put("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18] , 1);
+                    method_list.add("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18]);
+                }else{
+                    method_cnt.put("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18] , method_cnt.get("사고원인 : " + dataList.get(i)[14] + "\n\n해결방안 : "  + dataList.get(i)[18]) + 1);
+                }
+            }
+
+            List<String> real_method_list = new ArrayList<>();
+
+            for(int i =0; i < method_list.size(); i++){
+                String tmp_save = method_list.get(i);
+                method_list.remove(i);
+
+                real_method_list.add(tmp_save + " (" + method_cnt.get(tmp_save) + ")");
             }
 
             if(method_list.size() == 0){
                 method_list.add("선택한 분류에 해당되는 데이터가 없습니다.");
             }
 
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, method_list) ;
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, real_method_list) ;
 
             listView.setAdapter(adapter) ;
     }
